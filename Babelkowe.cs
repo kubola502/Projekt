b79df0c832b6caf60320f739400f80bba13f7393
Jakub Kacprzycki <148338540+kubola502@users.noreply.github.com>
2023-12-18 12:55:09 +0100

    Add files via upload

---
+++ Babelkowe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Aplikacja
+{
+    public class Babelkowe : Sortowanie_elementarne 
+    {
+        public override void Sortuj(int[] tablica)
+        {
+
+            int przechowalnia;
+            for (int i = 0; i < dlugosc_ciagu - 1; i++)
+            {
+                for (int j = 0; j < dlugosc_ciagu - 1; j++)
+                {
+                    if (tablica[j] > tablica[j + 1])
+                    {
+                        przechowalnia = tablica[j];
+                        tablica[j] = tablica[j + 1];
+                        tablica[j + 1] = przechowalnia;
+                    }
+                }
+            }
+        }
+        
+
+        
+    }
+}
